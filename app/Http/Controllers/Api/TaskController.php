f091305c8f9111011b8a359ecefad500e1b34f07
Roaa <roaatu42@github.com>
2026-08-13 23:32:47 +0300

    update TaskController

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -76,6 +76,7 @@ public function myTasks()
 
     $tasks = Task::with(['fixedTask', 'items.item'])
         ->where('staff_id', $staff->staff_id)
+        ->where('created_at','>=',now()->subHour(12))
         ->orderByDesc('id')
         ->get();
 
