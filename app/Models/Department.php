220a41870f537f377a3e7a0adae3bdca0d9c13c2
Roaa <roaatu42@github.com>
2026-08-14 01:08:37 +0300

    update Department

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -9,24 +9,26 @@ class Department extends Model
 {
     use HasFactory;
 
+    // تحديد اسم المفتاح الرئيسي لأن الاسم غير المعتاد (ليس id)
     protected $primaryKey = 'dep_id';
 
+    // الحقول المسموح بتعبئتها
     protected $fillable = [
-        'name'
-        //name_en
-        //name_ar
+        'name',
+        'name_ar',
+        'name_en'
     ];
 
+    // علاقة القسم بالموظفين (القسم يملك عدة موظفين)
     public function staff()
     {
-        return $this->hasMany(Staff::class,'dep_id');
+        return $this->hasMany(Staff::class, 'dep_id', 'dep_id');
     }
+
+    // علاقة القسم المهام الثابتة
     public function fixedTasks()
-{
-    return $this->hasMany(
-        FixedTask::class,
-        'dep_id',
-        'dep_id'
-    );
+    {
+        return $this->hasMany(FixedTask::class, 'dep_id', 'dep_id');
+    }
 }
-}
+   
